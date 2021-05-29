044f2c478805d060085afac170f1d4ed35969fe8
Juncheng Dong <jerry19951210@126.com>
2021-05-29 00:16:20 -0400

    Refectoring codes agin/ Added Reset Function

--- GUI2.py
+++ GUI2.py
@@ -2,7 +2,6 @@ from setting import *
 from helper import *
 
 import torch
-
 import numpy as np
 import pandas as pd
 
@@ -15,32 +14,15 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
-def init_param(num_lor):
-    w0 = torch.tensor(np.random.uniform(freq_low, freq_high, num_lor), requires_grad=True)
-    wp = torch.tensor(np.random.uniform(wp_freq_low, wp_freq_high, num_lor), requires_grad=True)
-    ws = torch.tensor(np.random.uniform(ws_freq_low, ws_freq_high, num_lor), requires_grad=True)
-    eps_inf = torch.tensor(5., requires_grad=True)
-    d = torch.tensor(50., requires_grad=True)
-    return w0, wp, ws, eps_inf, d
-
-def new_param():
-    ''' 
-        functin for generating one more set of Lorentzian parameters
-    '''
-    w0 = np.random.uniform(freq_low,freq_high)
-    wp = np.random.uniform(0,5)
-    ws = np.random.uniform(0,0.05)
-
-    return w0,wp,ws
+#global variable for reset the program
+RESET=False
 
 #Initialize Lorentzian parameters randomly for epsilon and mu
-w0,wp,ws,eps_inf,d=init_param(num_lor_init_epsilon) 
+w0,wp,ws,eps_inf=init_param(num_lor_init_epsilon) 
 w0[0]=3
 wp[0]=3
 ws[0]=0.01
-
-
-w0m,wpm,wsm,eps_infm,dm = init_param(num_lor_init_mu)
+w0m,wpm,wsm,eps_infm = init_param(num_lor_init_mu)
 
 #Store all related parameters in dict 'parameters'
 parameters={
@@ -48,14 +30,14 @@ parameters={
         "w0":list(w0),
         "ws":list(ws),
         "wp":list(wp),
-        "inf":eps_inf.item(),
+        "inf":eps_inf,
         "current_index":0
     },
     "mu":{
         "w0":list(w0m), 
         "ws":list(wsm),
         "wp":list(wpm),
-        "inf":eps_infm.item(),
+        "inf":eps_infm,
         "current_index":0
     },
     "epsilon_num_lor":num_lor_init_epsilon,
@@ -63,8 +45,10 @@ parameters={
     "nclick-epsilon":None,
     "nclick-mu":None,
     "num_spectra":NUM_SPECTRA,
-    "target_spectrum":None
+    "target_spectrum":None,
+    "nclick-reset":None
 }
+
 #generate equally separated points from 1THz to 5THz
 w=np.linspace(freq_low,freq_high,NUM_SPECTRA)
 
@@ -72,25 +56,29 @@ w=np.linspace(freq_low,freq_high,NUM_SPECTRA)
 app=dash.Dash(__name__)
 
 #region# ################### Epsilon Related Section ##########################
-epsilon_slider_area = update_epsilon_slider(parameters)
-epsilon_add_area=html.Div(className='slider',children=[
+epsilon_slider_area = html.Div(id='epsilon-slider-area',className='slider',
+    children = update_epsilon_slider(parameters)
+)
 
+epsilon_add_area=html.Div(className='slider',children=[
     dcc.Dropdown(
         id='epsilon-index-dd',
         options=[{'label': k+1, 'value': k} for k in range(parameters['epsilon_num_lor'])],
         value=0,style={"color":"black","text-align":"center"},
         clearable=False
     ),
-
     html.Button("Add",id='epsilon-add-button',className='button')
-    
 ])
-
 @app.callback(
     Output(component_id='epsilon-slider-area',component_property='children'),
-    Input(component_id='epsilon-index-dd',component_property='value')
-)
+    [Input(component_id='epsilon-index-dd',component_property='value')]
+) #callback for epsilon-index update
 def epsilon_slider_update(selected_index):
+    global RESET
+    if RESET:
+        return update_epsilon_slider(parameters)
+    
+    #if not in RESET, update the current index
     current_index=selected_index
     parameters['epsilon']['current_index']=current_index #update parameters dictionary
     return update_epsilon_slider(parameters)
@@ -98,21 +86,18 @@ def epsilon_slider_update(selected_index):
 
 @app.callback(
     [Output(component_id='epsilon-main-graph',component_property='figure'),
-    Output(component_id='epsilon-w0-slider-content',component_property='children'),
-    Output(component_id='epsilon-wp-slider-content',component_property='children'),
-    Output(component_id='epsilon-ws-slider-content',component_property='children'),
-    Output(component_id='epsilon-inf-slider-content',component_property='children'),
     Output(component_id='epsilon-index-dd',component_property='options'),
+    Output(component_id='epsilon-index-dd',component_property='value'),
     Output(component_id='epsilon-link',component_property='children')],
     
     [Input(component_id='epsilon-w0-slider',component_property='value'),
     Input(component_id='epsilon-wp-slider',component_property='value'),
     Input(component_id='epsilon-ws-slider',component_property='value'),
     Input(component_id='epsilon-inf-slider',component_property='value'),
-    Input('epsilon-add-button', 'n_clicks')]
+    Input('epsilon-add-button', 'n_clicks'),
+    Input('reset-link','children')]
 )
-def epsilon_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick):
-
+def epsilon_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick,resetlink):
     if nclick==None:
         pass
 
@@ -128,16 +113,17 @@ def epsilon_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick
         parameters['epsilon']['ws'].append(ws_new)
     
     current_index = parameters['epsilon']['current_index']
-    parameters['epsilon']['w0'][current_index]=selected_w0
-    parameters['epsilon']['wp'][current_index]=selected_wp
-    parameters['epsilon']['ws'][current_index]=selected_ws
-    parameters['epsilon']['inf']=selected_inf
 
+    if not RESET: #if not in RESET mode, update the parameters dictionary
+        parameters['epsilon']['w0'][current_index]=selected_w0
+        parameters['epsilon']['wp'][current_index]=selected_wp
+        parameters['epsilon']['ws'][current_index]=selected_ws
+        parameters['epsilon']['inf']=selected_inf
 
-    content1 = f'w_0: {selected_w0:.2f}'
-    content2 = f'w_p: {selected_wp:.2f}'
-    content3 = f'w_s: {selected_ws:.5f}'
-    content4 = f'inf: {selected_inf}'
+    if RESET:
+        dd_value = 0
+    else:
+        dd_value = current_index
 
 
     w0 = torch.tensor(parameters['epsilon']['w0'])
@@ -160,55 +146,59 @@ def epsilon_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick
     fig= go.Figure()
     fig.add_trace(go.Scatter(x=list(w),y=list(epi_r.real),name='real part'))
     fig.add_trace(go.Scatter(x=list(w),y=list(epi_r.imag),name='imaginary part'))
-    fig.add_trace(go.Scatter(x=[parameters['epsilon']['w0'][current_index]],y=[0],name='current peak',marker_size=16))
+
+    #plot the center the current Lorentzian
+    fig.add_trace(go.Scatter(x=[parameters['epsilon']['w0'][current_index]],y=[0],name='current peak',marker_size=14))
+    fig.add_trace(go.Scatter(x=parameters['epsilon']['w0'], y=[0]*num_lor,text=list(range(1,num_lor+1)),marker_size=8,name='Lorentzians',mode='markers',hovertemplate='Lor-index: <br>%{text}'))
+    
     fig.update_layout(legend=dict(
     yanchor="top",
     y=0.99,
     xanchor="right",
     x=0.99
     ),yaxis_range=[-200,200],
-    margin=dict(l=20, r=20, t=20, b=20))
+    margin=dict(l=5, r=5, t=5, b=5))
     
-    return fig, content1, content2, content3, content4, [{'label': k+1, 'value': k} for k in range(num_lor)], 'non-sense'
-
+    return fig, [{'label': k+1, 'value': k} for k in range(num_lor)],dd_value, 'non-sense'
 
 @app.callback(Output(component_id='epsilon-button-content',component_property='children'), [Input('epsilon-add-button', 'n_clicks')])
 def epsilon_on_click(nclick):
-    return html.Div(children=[f'total number of Lorentzians: ',html.Span(className='highlight',children=[f'{parameters["epsilon_num_lor"]}'])])
+    return html.Div(children=[f'EPSILON #Lorentzians: ',html.Span(className='highlight',children=[f'{parameters["epsilon_num_lor"]}'])])
 
 # #endregion#
 
 #region# ################### Mu Related Section ##########################
-mu_slider_area = update_mu_slider(parameters)
+mu_slider_area = html.Div(id='mu-slider-area',className='slider',
+    children = update_mu_slider(parameters)
+)
 mu_add_area=html.Div(className='slider',children=[
-
     dcc.Dropdown(
         id='mu-index-dd',
         options=[{'label': k+1, 'value': k} for k in range(parameters['mu_num_lor'])],
-        value=0,
+        value=0,style={"color":"black","text-align":"center"},
         clearable=False
     ),
-
     html.Button("Add",id='mu-add-button',className='button')
-    
 ])
+
 
 @app.callback(
     Output(component_id='mu-slider-area',component_property='children'),
     Input(component_id='mu-index-dd',component_property='value')
 )
-def mu_update_index(selected_index):
+def mu_slider_index(selected_index):
+    global RESET
+    if RESET:
+        return update_mu_slider(parameters)
+
     current_index=selected_index
     parameters['mu']['current_index']=current_index
     return update_mu_slider(parameters)
 
 @app.callback(
     [Output(component_id='mu-main-graph',component_property='figure'),
-    Output(component_id='mu-w0-slider-content',component_property='children'),
-    Output(component_id='mu-wp-slider-content',component_property='children'),
-    Output(component_id='mu-ws-slider-content',component_property='children'),
-    Output(component_id='mu-inf-slider-content',component_property='children'),
     Output(component_id='mu-index-dd',component_property='options'),
+    Output(component_id='mu-index-dd',component_property='value'),
     Output(component_id='mu-link',component_property='children'),
     Output(component_id='mu-slider-area',component_property='className')],
     
@@ -216,9 +206,10 @@ def mu_update_index(selected_index):
     Input(component_id='mu-wp-slider',component_property='value'),
     Input(component_id='mu-ws-slider',component_property='value'),
     Input(component_id='mu-inf-slider',component_property='value'),
-    Input('mu-add-button', 'n_clicks')]
+    Input('mu-add-button', 'n_clicks'),
+    Input('reset-link','children')]
 )
-def mu_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick):
+def mu_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick,resetlink):
 
     if nclick==None:
         pass
@@ -226,7 +217,6 @@ def mu_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick):
     if nclick != parameters['nclick-mu']:
         parameters['nclick-mu'] = nclick
         parameters['mu_num_lor'] = parameters['mu_num_lor']+1
-        # parameters['epsilon']['current_index'] = parameters['num_lor']+1
 
         #adding parameters
         if parameters['mu_num_lor']==1:
@@ -243,11 +233,17 @@ def mu_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick):
     parameters['mu']['ws'][current_index]=selected_ws
     parameters['mu']['inf']=selected_inf
 
+    if not RESET: #if not in RESET mode, update the parameters dictionary
+        parameters['mu']['w0'][current_index]=selected_w0
+        parameters['mu']['wp'][current_index]=selected_wp
+        parameters['mu']['ws'][current_index]=selected_ws
+        parameters['mu']['inf']=selected_inf
 
-    content1 = f'w_0: {selected_w0:.2f}'
-    content2 = f'w_p: {selected_wp:.2f}'
-    content3 = f'w_s: {selected_ws:.5f}'
-    content4 = f'inf: {selected_inf}'
+    if RESET:
+        dd_value = 0
+    else:
+        dd_value = current_index
+
 
 
     w0 = torch.tensor(parameters['mu']['w0'])
@@ -255,7 +251,7 @@ def mu_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick):
     ws = torch.tensor(parameters['mu']['ws'])
     eps_inf = torch.tensor(parameters['mu']['inf'])
 
-    num_spectra=2001
+    num_spectra=NUM_SPECTRA
     num_lor = parameters['mu_num_lor']
 
     w0 = w0.unsqueeze(1).expand(num_lor, num_spectra)
@@ -278,76 +274,30 @@ def mu_update_graph(selected_w0,selected_wp,selected_ws,selected_inf,nclick):
         pass
     else:
         fig.add_trace(go.Scatter(x=[parameters['mu']['w0'][current_index]],y=[0],name='current peak',marker_size=16))
+        fig.add_trace(go.Scatter(x=parameters['mu']['w0'], y=[0]*num_lor,text=list(range(1,num_lor+1)),marker_size=8,name='Lorentzians',mode='markers',hovertemplate='Lor-index: <br>%{text}'))
     fig.update_layout(legend=dict(
     yanchor="top",
     y=0.99,
     xanchor="right",
     x=0.99
-    ),  ##yaxis_range=[-200,200],
-    margin=dict(l=20, r=20, t=20, b=20))
+    ),  
+    margin=dict(l=20, r=10, t=10, b=20))
 
-    'a'
     if num_lor == 0:
         className = 'slider invisible'
     else:
         className = 'slider'
     
-    return fig, content1, content2, content3, content4, [{'label': k+1, 'value': k} for k in range(num_lor)], 'non-sense',className
+    return fig, [{'label': k+1, 'value': k} for k in range(num_lor)], dd_value, 'non-sense',className
 
 
 @app.callback(Output(component_id='mu-button-content',component_property='children'), [Input('mu-add-button', 'n_clicks')])
 def mu_on_click(nclick):
-    return html.Div(children=[f'total number of Lorentzians: ',html.Span(className='highlight',children=[f'{parameters["mu_num_lor"]}'])])
+    return html.Div(children=[f'MU #Lorentzians: ',html.Span(className='highlight',children=[f'{parameters["mu_num_lor"]}'])])
 
 # #endregion#
 
-control_area_children = [
-    html.Div(id='epsilon-control',children=[
-        dcc.Graph(id='epsilon-main-graph',figure={}),
-        html.Div(id='epsilon-control-right',children=[epsilon_slider_area,
-        epsilon_add_area
-        ]),
-        html.Div(id='epsilon-button-content')
-    ]),
-
-    html.Div(id='mu-control',children=[
-        dcc.Graph(id='mu-main-graph',figure={}),
-        html.Div(id='mu-control-right',children=[mu_slider_area,
-        mu_add_area
-        ]),
-        html.Div(id='mu-button-content')
-    ]),
-
-    #these two divs are just for control 
-    html.Div(id='mu-link',style={"display":"none"}),
-    html.Div(id='epsilon-link',style={"display":"none"})
-]
-
-display_area_children=[
-    dcc.Graph(id='n-graph',className='side-plot',figure={}),
-    dcc.Graph(id='z-graph',className='side-plot',figure={})
-]
-
-main_area_children=[
-    dcc.Graph(id='T-graph',figure={})
-]
-
-title_bar_children=[
-    upload_component,
-    
-    html.H1(children=[html.Span("Juncheng",style={"text-decoration":"underline"}),"App"],style={"text-align":"center"}),
-    reset_component,
-    html.Div(id='upload-link',style={"display":"none"})
-]
-
-app.layout=html.Div([
-    html.Div(id='title-bar',children=title_bar_children),
-    html.Div(id='main-display-area',children=main_area_children),
-    html.Div(id='display-area',children=display_area_children),html.Div(style={"clear":"both"}),
-    html.Div(id='control-area',children = control_area_children)
-])
-
-
+#region# ################### Main Plot/Load/Reset #########################
 @app.callback(
     [Output(component_id='n-graph',component_property='figure'),
     Output(component_id='z-graph',component_property='figure'),
@@ -405,11 +355,12 @@ def display_update_graph(linktext1,linktext2,linktext3):
     n = torch.sqrt(epi_r*mu_r).detach()
     z = torch.sqrt(mu/epsilon).detach()
 
-    k=torch.tensor(w)*2*np.pi*n/c #wn/c
+    k=100*torch.tensor(w)*2*np.pi*n/c #wn/c
     t = 1/(torch.cos(k*d)-1j/2*(z+torch.pow(z,-1))*torch.sin(k*d))
     r = -1j/2*(z-torch.pow(z,-1)*torch.sin(k*d))*t
 
     T = t.real**2 + t.imag**2
+    T2= torch.exp(-2*k.imag*d)* ( (n/mu_r).real * torch.abs((2*mu_r)/(n+mu_r))**2)**2
 
     fig_n= go.Figure()
     fig_n.add_trace(go.Scatter(x=list(w),y=list(n.real),name='real part'))
@@ -431,6 +382,7 @@ def display_update_graph(linktext1,linktext2,linktext3):
 
     fig_T= go.Figure()
     fig_T.add_trace(go.Scatter(x=list(w),y=list(T.detach()),name='constructed'))
+    fig_T.add_trace(go.Scatter(x=list(w),y=list(T2.detach()),name='T2'))
     if not parameters['target_spectrum'] is None:
         fig_T.add_trace(go.Scatter(x=list(w),y=list(parameters['target_spectrum']),name='target',
         line = dict(color='grey', width=2, dash='dash')))
@@ -442,6 +394,9 @@ def display_update_graph(linktext1,linktext2,linktext3):
     ),title="Spectrum - T", title_x=0.5,height=400,yaxis_range=[-0.05,1],
     margin=dict(l=15, r=15, t=30, b=15))
 
+    global RESET
+    RESET=False
+
     return fig_n,fig_z,fig_T
     
 @app.callback(
@@ -450,16 +405,81 @@ def display_update_graph(linktext1,linktext2,linktext3):
               Input('upload-data', 'contents'),
               State('upload-data', 'filename'),
               State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
+def update_load(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
         data,children = parse_contents(list_of_contents, list_of_names, list_of_dates)
         # [parse_contents(c, n, d) for c, n, d in
         #     zip(list_of_contents, list_of_names, list_of_dates)]
         parameters['target_spectrum'] = data[0]
-        print(data[0])
         return children,'nonsense'
     else:
         return None,'nonsense'
+
+@app.callback(
+    Output('reset-link','children'),
+    Input('reset-area','n_clicks')
+)
+def update_reset(reset_nclicks):
+    global RESET
+    print("reset_nclicks:",reset_nclicks)
+    if reset_nclicks != parameters['nclick-reset']:
+        RESET=True
+        temp_parameters = reset_parameters(reset_nclicks,parameters)
+
+        for key in temp_parameters:
+            parameters[key] = temp_parameters[key]
+    
+    return 'nonsense'
+# #endregion#
+
+#region#  ####################### App Layout Section ########################
+control_area_children = [
+    html.Div(id='epsilon-control',children=[
+        dcc.Graph(id='epsilon-main-graph',figure={}),
+        html.Div(id='epsilon-control-right',children=[epsilon_slider_area,
+        epsilon_add_area
+        ]),
+        html.Div(id='epsilon-button-content')
+    ]),
+
+    html.Div(id='mu-control',children=[
+        dcc.Graph(id='mu-main-graph',figure={}),
+        html.Div(id='mu-control-right',children=[mu_slider_area,
+        mu_add_area
+        ]),
+        html.Div(id='mu-button-content')
+    ]),
+
+    #these two divs are just for control 
+    html.Div(id='mu-link',style={"display":"none"}),
+    html.Div(id='epsilon-link',style={"display":"none"})
+]
+
+display_area_children=[
+    dcc.Graph(id='n-graph',className='side-plot',figure={}),
+    dcc.Graph(id='z-graph',className='side-plot',figure={})
+]
+
+main_area_children=[
+    dcc.Graph(id='T-graph',figure={})
+]
+
+title_bar_children=[
+    upload_component,
+    
+    html.H1(children=[html.Span("Juncheng",style={"text-decoration":"underline"}),"App"],style={"text-align":"center"}),
+    reset_component,
+    html.Div(id='upload-link',style={"display":"none"}),
+    html.Div(id='reset-link',style={"display":"none"})
+]
+
+app.layout=html.Div([
+    html.Div(id='title-bar',children=title_bar_children),
+    html.Div(id='main-display-area',children=main_area_children),
+    html.Div(id='display-area',children=display_area_children),html.Div(style={"clear":"both"}),
+    html.Div(id='control-area',children = control_area_children)
+])
+# #endregion#
 
 #Finally! Run the Server
 app.run_server(debug=True)
--- helper.py
+++ helper.py
@@ -6,6 +6,8 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 import pandas as pd
+import numpy as np
+import torch
 
 import base64
 import datetime
@@ -16,12 +18,18 @@ def update_epsilon_slider(parameters):
     '''
     update_slider will return a dash component for epsilon sliders 
     this function will be reused for code efficiency
+
+    contains: 1.ws slider 2.wp slider 3.w0 slider 4. epsilon_inf slider
     '''
     epsilon_current_index = parameters["epsilon"]["current_index"]
+    selected_w0 = parameters['epsilon']['w0'][epsilon_current_index]
+    selected_wp = parameters['epsilon']['wp'][epsilon_current_index]
+    selected_ws = parameters['epsilon']['ws'][epsilon_current_index]
+    selected_inf = parameters['epsilon']['inf']
 
-    return html.Div(id='epsilon-slider-area',className='slider',children=[
-        
-        html.Div(id='epsilon-w0-slider-content',className='slider-content',children=[],style={"text-align":"center","font-size":"1.5em"}),
+
+    return [
+        html.Div(id='epsilon-w0-slider-content',className='slider-content',children=[f'w0: {selected_w0:.2f}']),
         dcc.Slider(
             id='epsilon-w0-slider',
             min=w0_freq_low,
@@ -36,7 +44,7 @@ def update_epsilon_slider(parameters):
             # ,updatemode='drag'
         ),
         
-        html.Div(id='epsilon-wp-slider-content',className='slider-content',children=[],style={"text-align":"center","font-size":"1.5em"}),
+        html.Div(id='epsilon-wp-slider-content',className='slider-content',children=[f'wp: {selected_wp:.2f}']),
         dcc.Slider(
             id='epsilon-wp-slider',
             min=wp_freq_low,
@@ -50,7 +58,7 @@ def update_epsilon_slider(parameters):
             }
         ),
         
-        html.Div(id='epsilon-ws-slider-content',className='slider-content',children=[],style={"text-align":"center","font-size":"1.5em"}),
+        html.Div(id='epsilon-ws-slider-content',className='slider-content',children=[f'ws: {selected_ws:.3f}']),
         dcc.Slider(
             id='epsilon-ws-slider',
             min=ws_freq_low,
@@ -64,7 +72,7 @@ def update_epsilon_slider(parameters):
             }
         ),
         
-        html.Div(id='epsilon-inf-slider-content',className='slider-content',style={"text-align":"center","font-size":"1.5em"}),
+        html.Div(id='epsilon-inf-slider-content',className='slider-content',children=[f'inf: {selected_inf}']),
         dcc.Slider(
             id='epsilon-inf-slider',
             min=inf_low,
@@ -77,7 +85,7 @@ def update_epsilon_slider(parameters):
                 inf_high:{"label":f"max:{inf_high}",'style': {'color': 'white'}}
             }
         )
-    ])
+    ]
 
 def update_mu_slider(parameters):
     '''
@@ -85,10 +93,13 @@ def update_mu_slider(parameters):
     this function will be reused for code efficiency
     '''
     mu_current_index = parameters["mu"]["current_index"]
+    selected_w0 = parameters['mu']['w0'][mu_current_index]
+    selected_wp = parameters['mu']['wp'][mu_current_index]
+    selected_ws = parameters['mu']['ws'][mu_current_index]
+    selected_inf = parameters['mu']['inf']
 
-    return html.Div(id='mu-slider-area',className='slider',children=[
-        
-        html.Div(id='mu-w0-slider-content',className='slider-content',children=[],style={"text-align":"center","font-size":"1.5em"}),
+    return [
+        html.Div(id='mu-w0-slider-content',className='slider-content',children=[f'w_0 : {selected_w0:.2f}'],style={"text-align":"center","font-size":"1.5em"}),
         dcc.Slider(
             id='mu-w0-slider',
             min=w0_freq_low,
@@ -103,7 +114,7 @@ def update_mu_slider(parameters):
             # ,updatemode='drag'
         ),
         
-        html.Div(id='mu-wp-slider-content',className='slider-content',children=[],style={"text-align":"center","font-size":"1.5em"}),
+        html.Div(id='mu-wp-slider-content',className='slider-content',children=[f'w_p : {selected_wp:.2f}'],style={"text-align":"center","font-size":"1.5em"}),
         dcc.Slider(
             id='mu-wp-slider',
             min=wp_freq_low,
@@ -117,7 +128,7 @@ def update_mu_slider(parameters):
             }
         ),
         
-        html.Div(id='mu-ws-slider-content',className='slider-content',children=[],style={"text-align":"center","font-size":"1.5em"}),
+        html.Div(id='mu-ws-slider-content',className='slider-content',children=[f'w_s : {selected_ws:.3f}'],style={"text-align":"center","font-size":"1.5em"}),
         dcc.Slider(
             id='mu-ws-slider',
             min=ws_freq_low,
@@ -131,7 +142,7 @@ def update_mu_slider(parameters):
             }
         ),
         
-        html.Div(id='mu-inf-slider-content',className='slider-content',style={"text-align":"center","font-size":"1.5em"}),
+        html.Div(id='mu-inf-slider-content',className='slider-content',children=[f'inf: {selected_inf}'],style={"text-align":"center","font-size":"1.5em"}),
         dcc.Slider(
             id='mu-inf-slider',
             min=inf_low,
@@ -144,7 +155,7 @@ def update_mu_slider(parameters):
                 inf_high:{"label":f"max:{inf_high}",'style': {'color': 'white'}}
             }
         )
-    ])
+    ]
 
 reset_component = html.Div('Clear&Reset',id='reset-area')
 
@@ -201,3 +212,60 @@ def parse_contents(contents, filename, date):
         #     'wordBreak': 'break-all'
         # })
     ])
+
+def init_param(num_lor):
+    w0 = np.random.uniform(freq_low, freq_high, num_lor)
+    wp = np.random.uniform(wp_freq_low, wp_freq_high, num_lor)
+    ws = np.random.uniform(ws_freq_low, ws_freq_high, num_lor)
+    eps_inf = 1.
+    return w0, wp, ws, eps_inf
+def new_param():
+    ''' 
+        functin for generating one more set of Lorentzian parameters
+    '''
+    w0 = np.random.uniform(freq_low,freq_high)
+    wp = np.random.uniform(0,5)
+    ws = np.random.uniform(0,0.05)
+    return w0,wp,ws
+
+def reset_parameters(reset_nclicks,old_parameters=None):
+    #Initialize Lorentzian parameters randomly for epsilon and mu
+    w0,wp,ws,eps_inf=init_param(num_lor_init_epsilon) 
+    w0[0]=3
+    wp[0]=3
+    ws[0]=0.01
+    w0m,wpm,wsm,eps_infm = init_param(num_lor_init_mu)
+
+    if old_parameters is  None:
+        nclick_epsilon=None
+        nclick_mu=None
+    else:
+        nclick_epsilon =  old_parameters['nclick-epsilon']
+        nclick_mu = old_parameters['nclick-mu']
+
+    #Store all related parameters in dict 'parameters'
+    parameters={
+        "epsilon":{
+            "w0":list(w0),
+            "ws":list(ws),
+            "wp":list(wp),
+            "inf":eps_inf,
+            "current_index":0
+        },
+        "mu":{
+            "w0":list(w0m), 
+            "ws":list(wsm),
+            "wp":list(wpm),
+            "inf":eps_infm,
+            "current_index":0
+        },
+        "epsilon_num_lor":num_lor_init_epsilon,
+        "mu_num_lor":0,
+        "nclick-epsilon":nclick_epsilon,
+        "nclick-mu":nclick_mu,
+        "num_spectra":NUM_SPECTRA,
+        "target_spectrum":None,
+        "nclick-reset":reset_nclicks
+    }
+
+    return parameters
